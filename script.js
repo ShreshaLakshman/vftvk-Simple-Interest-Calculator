fce9af533b6c6fc47f22cf4536a876e58995f94c
Shresha <shreshalakshman22@gmail.com>
2022-01-03 10:11:33 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p,t,r,year,si;
+    p = document.getElementById ("principal").value;
+    t = document.getElementById ("years").value;
+    r = document.getElementById ("rate").value;
+    si = parseInt((p*t*r)/100 );
+    year = new Date().getFullYear()+parseInt(years);
+    document.getElementById('num').innerHTML="if you deposit" +p+ <br>"at an interest rate of" +r+</br> 
+    "you will receive an amount of " +si+ "in the year" +year;
+
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+
+
+
