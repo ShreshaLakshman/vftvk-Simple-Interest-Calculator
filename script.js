f485dd78e4a52ab8fcbf11eddf75ab03cdaa205f
ShreshaLakshman <96971594+ShreshaLakshman@users.noreply.github.com>
2022-01-13 11:45:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 function compute()
 {
-    var p,t,r,year,si;
-    p = document.getElementById ("principal").value;
-    t = document.getElementById ("years").value;
-    r = document.getElementById ("rate").value;
-    si = parseInt((p*t*r)/100 );
-    year = new Date().getFullYear()+parseInt(years);
-    document.getElementById('num').innerHTML="if you deposit" +p+ <br>"at an interest rate of" +r+</br> 
-    "you will receive an amount of " +si+ "in the year" +year;
-
+    let p = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = p * years * rate /100;
+    let year = new Date().getFullYear()+parseInt(years);
+    let result = document.getElementById("result")
+    if (p <= 0) 
+    {
+        document.getElementById("principal").value = "";
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } 
+    else
+    {
+        result.innerHTML = `
+        If you deposit <span style="background-color: yellow;">${p}</span>,<br/>
+        at an interest rate of <span style="background-color: yellow;">${rate}%</span>.<br/>
+        You will receive an amount of <span style="background-color: yellow;">${interest}</span>,<br/>
+        in the year <span style="background-color: yellow;">${year}</span>
+        `    
+    };
 }
+
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-
-
-
 
